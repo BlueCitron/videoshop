217df124ab8c4b33b993c7d670d72220172e59e0
BlueCitron <sds901234@naver.com>
2020-03-25 22:53:20 +0900

    Initial commit

---
+++ src/main/java/study/videoshop/VideoshopApplication.java
@@ -0,0 +1,13 @@
+package study.videoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoshopApplication.class, args);
+	}
+
+}
